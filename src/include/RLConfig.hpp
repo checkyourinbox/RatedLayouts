da859985b845839a85cff77da33f145c74857311
eightfold <yougotmmmail@gmail.com>
2026-07-16 23:09:20 -0400

    Formatting

--- src/include/RLConfig.hpp
+++ src/include/RLConfig.hpp
@@ -4,9 +4,9 @@
 // Various attribute macros
 
 #if defined(GEODE_IS_WINDOWS)
-# define RL_NOINLINE __declspec(noinline)
+# define RL_NO_INLINE __declspec(noinline)
 # define RL_ALWAYS_INLINE __forceinline
 #else
-# define RL_NOINLINE __attribute__((noinline))
+# define RL_NO_INLINE __attribute__((noinline))
 # define RL_ALWAYS_INLINE __attribute__((always_inline)) inline
 #endif
