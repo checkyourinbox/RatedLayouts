ef0810bf177c87ff0549f155fe5aae5e4c6c73fd
eightfold <yougotmmmail@gmail.com>
2026-09-07 19:10:08 -0400

    1.0.17

---
+++ src/utils/StartupFunctions.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace rl {
+
+/// In `utils/CachedSettings.cpp`
+void CachedSettings_init();
+/// In `utils/RLData.cpp`
+void RLData_init();
+/// In `layer/RLShopLayer2.cpp`
+void ShopLayer_init(); // Get the sizes at startup
+void ShopLayer_prefetch(); // Start loading the pages when checking the main menu
+/// In `hooks/Badges.cpp`
+bool Badges_init();
+
+void GetAllServerData_fetch();
+
+} // namespace rl
